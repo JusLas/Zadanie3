0ae7fb0d45f80ff28cdbdd31dae0390cf049ccc6
Justyna <justyna.laszuk@gmail.com>
2021-11-07 19:14:34 +0100

    liczba produktow

--- zadanie3_2.py
+++ zadanie3_2.py
@@ -3,4 +3,12 @@ lista_zakupow={
     "piekarnia":["bułka","chleb","paczek"]
 }
 for sklep in lista_zakupow:
-    print(f"Idę do {sklep.capitalize()}, kupuję tam astępujące rzeczy {[item.capitalize() for item in lista_zakupow[sklep]]}")  
+    print(f"Idę do {sklep.capitalize()}, kupuję tam astępujące rzeczy {[item.capitalize() for item in lista_zakupow[sklep]]}") 
+
+liczba_produktow = 0
+for sklep in lista_zakupow:
+    liczba_produktow = liczba_produktow + len(lista_zakupow[sklep])   
+print(f"W sumie kupuję {liczba_produktow} produktow")
+
+zakupy = lista_zakupow["warzywaniak"]
+print(zakupy)
