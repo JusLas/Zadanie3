4248d671885249024f3072175f4b33b7642c303e
Justyna <justyna.laszuk@gmail.com>
2021-11-07 18:53:00 +0100

    initial commit

--- zadanie3_2.py
+++ zadanie3_2.py
@@ -1,1 +1,6 @@
-"rozwiazanie zadania_ z modulu_3"
+lista_zakupow={
+    "warzywaniak": ["cebula","zieminaki", "buraki"],
+    "piekarnia":["bułka","chleb","paczek"]
+}
+for sklep in lista_zakupow:
+    print(f"Idę do {sklep}, kupuję tam astępujące rzeczy {lista_zakupow[sklep]}")
